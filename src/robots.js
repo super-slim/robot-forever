8cdf8e4d640b76441a486cbb9be4caa3f5bfa3d5
Sepehr <sepehr.salim@gmail.com>
2020-07-27 14:03:15 +0430

    Initializing the best Website

---
+++ src/robots.js
@@ -0,0 +1,65 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Rimes',
+	username: 'Bret',
+	email: 'leanne.rimes@blonde.com'
+},
+{
+	id: 2,
+	name: 'Jack Palett',
+	username: 'Nikoo',
+	email: 'jack.pal@mancho.com'
+},
+{
+	id: 3,
+	name: 'Mike Finn',
+	username: 'Mickey',
+	email: 'mikefinn69@loobak.com'
+},
+{
+	id: 4,
+	name: 'Graham Green',
+	username: 'Buzz',
+	email: 'buz.gg@hamm.com'
+},
+{
+	id: 5,
+	name: 'Mary Monini',
+	username: 'Nini',
+	email: 'nini.mon@zarra.com'
+},
+{
+	id: 6,
+	name: 'Julia Smith',
+	username: 'Smitty',
+	email: 'julia.smith@blonde.com'
+},
+{
+	id: 7,
+	name: 'Fred Ward',
+	username: 'Fufy',
+	email: 'fufyfufy@mancho.com'
+},
+{
+	id: 8,
+	name: 'Sara Davis',
+	username: 'Dred',
+	email: 'saradred@loobak.com'
+},
+{
+	id: 9,
+	name: 'Bob North',
+	username: 'Xman',
+	email: 'bobbynorth@hamm.com'
+},
+{
+	id: 10,
+	name: 'Lily Konka',
+	username: 'Kong',
+	email: 'liliana@zarra.com'
+}
+];
+
+
+
